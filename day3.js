8688564b8625d5a71b26d91e2a48f542270f0a16
A-Hakos <adhithyaz12@gmail.com>
2026-02-24 19:59:19 -0800

    add: new learning fundamental

---
+++ day3.js
@@ -0,0 +1,14 @@
+/* 
+Else if condition : digunakan saat ada dua atau lebih kondisi 
+ example : */
+let nilai = 60;
+
+if (nilai >= 90) {
+  console.log("Grade A");
+} else if (nilai >= 80) {
+  console.log("Grade B");
+} else if (nilai >= 70) {
+  console.log("Grade C");
+} else {
+  console.log("Grade D");
+}
